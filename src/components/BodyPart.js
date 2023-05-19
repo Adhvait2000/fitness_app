fb9e03870d3936668c16f65413e46fa85bd1b7d6
Adhvait2000 <64685259+Adhvait2000@users.noreply.github.com>
2023-05-20 01:46:18 +0800

    BodyPart

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -1,7 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { Stack, Typography} from '@mui/material';
 
-export const BodyPart = () => {
+import Icon from '../assets/icons/gym.png';
+
+export const BodyPart = ({item, setBodyPart, bodyPart}) => {
   return (
-    <div>BodyPart</div>
+    <Stack
+        type = "button"
+        alignItems="center"
+        justifyContent = "center"
+        className = "bodyPart-card"
+        sx={{
+            borderTop: bodyPart === item ? '4px solid #ff2625' : '',
+            backgroundColor: '#fff',
+            borderBottomLeftRadius: '20px',
+            width: '270px',
+            height: '280px',
+            cursour: 'pointer',
+            gap: '47px' 
+        }}
+        onClick = {()=>{
+            setBodyPart(item);
+            window.scrollTo({top:1800, left:100, behavior: 'smooth'})
+        }}
+    >
+        <img src={Icon} alt="dumbbell" style={{width:'40px', height:'40px'}} />
+        <Typography fontSize="24px" fontWeight="bold" color="#3A1212" textTransform="capitalize">{item}</Typography>
+    </Stack>
   )
 }
