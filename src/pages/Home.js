3106354d282228eda867a458ac4e80c8e29d5bc1
Adhvait2000 <64685259+Adhvait2000@users.noreply.github.com>
2023-05-20 00:38:58 +0800

    Start Page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react';
+import { Box } from '@mui/material';
+
+import Exercises from '../components/Exercises';
+import HeroBanner from '../components/HeroBanner';
+import SearchExercises from '../components/SearchExercises';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Box>
+        <HeroBanner />
+        <SearchExercises />
+        <Exercises />
+    </Box>
   )
 }
 
