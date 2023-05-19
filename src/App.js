3cb330f4658f86afc8e5afc644b77e59c98e504c
Adhvait2000 <64685259+Adhvait2000@users.noreply.github.com>
2023-05-20 00:23:54 +0800

    NavBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material';
 import './App.css';
+import ExerciseDetail from './pages/ExerciseDetail';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <Box width="400px" sx={{width: { xl:'1488px'}}} m = "auto">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/exercise/:id" element={<ExerciseDetail />} />
+        </Routes>
+        <Footer />
+    </Box>
+  );
+};
 
-export default App
+export default App;
