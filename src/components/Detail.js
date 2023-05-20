aaf501bc88b742d06c9651ac738c7accdb91b0c7
Adhvait2000 <64685259+Adhvait2000@users.noreply.github.com>
2023-05-20 12:53:25 +0800

    Exercise Detail initial

---
+++ src/components/Detail.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Typography, Stack, Button } from '@mui/material';
+
+import BodyPartImage from '../assets/icons/body-part.png';
+import TargetImage from '../assets/icons/target.png';
+import EquipmentImage from '../assets/icons/equipment.png';
+
+const Detail = (exerciseDetail) => {
+    const{bodyPart, gifUrl, name, target, equipment} = exerciseDetail;
+
+    console.log(gifUrl);
+
+    return (
+        <Stack gap="60px" sx={{flexDirection: {lg:'row'}, p:'20px', alignItms: 'center'}}>
+            <img src={gifUrl} alt={name} loading="lazy" className="detail-image" />
+
+
+        </Stack>
+    )
+}
+
+export default Detail
